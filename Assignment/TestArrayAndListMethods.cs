9e8b0361d1c114c809359fbce18df58c19c4f794
eirikkar <eirikkar@proton.me>
2024-12-26 14:26:08 +0100

    tried to implement binary search

--- Assignment/TestArrayAndListMethods.cs
+++ Assignment/TestArrayAndListMethods.cs
@@ -66,7 +66,27 @@ public class TestArrayAndListMethods : AssignmentBase
         {
             throw new Exception();
         }
-        throw new NotImplementedException();
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == x)
+            {
+                return x;
+            }
+            else if (arr[mid] < x)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return x;
     }
 
     /// <summary>
