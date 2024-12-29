6d0068f2a9f04891ce7a5ee32a0d9b1295d4ee5e
eirikkar <eirikkar@proton.me>
2024-12-29 22:43:05 +0100

    implemented ShouldBeStringArray

--- Assignment/TestDatastructures.cs
+++ Assignment/TestDatastructures.cs
@@ -5,7 +5,6 @@ using Xunit;
 
 public class TestDatastructures : AssignmentBase
 {
-
     Datastructures datastructures = new Datastructures()
     {
         Strings = [],
@@ -15,47 +14,55 @@ public class TestDatastructures : AssignmentBase
         Doubles2d = [],
     };
 
-
     [Assignment(1)]
     public void ShouldBeStringArray()
     {
+        datastructures.Strings = new string[5];
         Assert.Equal(datastructures.Strings, new string[5]);
     }
+
     [Assignment(2)]
     public void ShouldBeIntArray()
     {
         Assert.Equal(datastructures.Integers, new int[5]);
     }
+
     [Assignment(3)]
     public void ShouldBeDoubleArray()
     {
         Assert.Equal(datastructures.Doubles, new double[5]);
     }
+
     [Assignment(4)]
     public void ShouldBeBooleanArray()
     {
         Assert.Equal(datastructures.Booleans, new bool[2]);
     }
+
     [Assignment(5)]
     public void ShouldBe2DStringArray()
     {
         Assert.Equal(datastructures.Strings2d, new string[1][]);
     }
+
     [Assignment(6)]
     public void ShouldBe2DIntArray()
     {
         Assert.Equal(datastructures.Integers2d, new int[1][]);
     }
+
     [Assignment(7)]
     public void ShouldBe2DArrayOfDoubles()
     {
         Assert.Equal(datastructures.Doubles2d, new double[1][]);
     }
+
     [Assignment(8)]
     public void ShouldBeListOfStrings()
     {
         Assert.Equal(new List<string>(), datastructures.StringsList);
     }
+
     [Assignment(9)]
     public void ShouldBeDictionaryOfIntAndStrings()
     {
